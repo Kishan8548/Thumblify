0afbeff5324176d0a69e5b4f19c8d6905d64cbcb
Kishan Garhwal <surendrakuma123@gmail.com>
2026-01-25 16:29:36 +0530

    feat: Add thumbnail generation and user management features
    - Integrated Google GenAI for thumbnail generation with customizable styles and color schemes.
    - Implemented Thumbnail model to store generated thumbnails and their metadata.
    - Created ThumbnailController for generating and deleting thumbnails.
    - Added UserController to manage user-specific thumbnail retrieval.
    - Established new routes for thumbnail and user operations, including authentication middleware for secure access.
    - Updated package.json to include necessary dependencies for AI and cloud storage.

--- server/routes/AuthRoutes.ts
+++ server/routes/AuthRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { loginUser, logoutUser, registerUser, verifyUser } from "../controllers/AuthController.js";
+import protect from "../middlewares/auth.js";
 
 const AuthRouter = express.Router();
 
 AuthRouter.post('/register', registerUser)
 AuthRouter.post('/login', loginUser)
-AuthRouter.get('/verify', verifyUser)
-AuthRouter.post('/logout', logoutUser)
+AuthRouter.get('/verify',protect, verifyUser)
+AuthRouter.post('/logout',protect, logoutUser)
 
 export default AuthRouter
